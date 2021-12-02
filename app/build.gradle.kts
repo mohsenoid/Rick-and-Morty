8a0d7da8a448fb54a850c4057ae7334041b3a929
Mohsen <msn.mirhoseini@gmail.com>
2021-12-02 21:40:09 +0100

    Update OkHttp
    Signed-off-by: Mohsen <msn.mirhoseini@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -162,7 +162,7 @@ dependencies {
     // Retrofit
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:0.8.0")
-    val okhttpVersion = "4.9.2"
+    val okhttpVersion = "4.9.3"
     implementation("com.squareup.okhttp3:okhttp:$okhttpVersion")
     implementation("com.squareup.okhttp3:logging-interceptor:$okhttpVersion")
 
