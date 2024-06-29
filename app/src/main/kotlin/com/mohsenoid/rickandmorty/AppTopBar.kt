50cb57d1737c50e6fc47d7d33f1622672584b53b
Mohsen Mirhoseini <msn.mirhoseini@gmail.com>
2024-06-29 15:02:33 +0200

    Separate App Top Bar

---
+++ app/src/main/kotlin/com/mohsenoid/rickandmorty/AppTopBar.kt
@@ -0,0 +1,80 @@
+package com.mohsenoid.rickandmorty
+
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.mohsenoid.rickandmorty.ui.NavRoute
+import com.mohsenoid.rickandmorty.ui.theme.RickAndMortyTheme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AppTopBar(
+    navController: NavController,
+    modifier: Modifier = Modifier,
+) {
+    val currentBackStackEntry by navController.currentBackStackEntryFlow.collectAsStateWithLifecycle(
+        initialValue = navController.currentBackStackEntry,
+    )
+    TopAppBar(
+        title = {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Text(
+                    text = stringResource(id = R.string.app_name),
+                    color = MaterialTheme.colorScheme.onSurface,
+                    style = MaterialTheme.typography.titleLarge,
+                )
+            }
+        },
+        modifier = modifier,
+        navigationIcon = {
+            if (currentBackStackEntry?.destination?.route != NavRoute.EpisodesScreen.route) {
+                IconButton(onClick = { navController.popBackStack() }) {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                        contentDescription = "Back",
+                        tint = MaterialTheme.colorScheme.onSurface,
+                    )
+                }
+            } else {
+                Icon(
+                    painter = painterResource(id = R.drawable.rick_morty),
+                    contentDescription = "Home",
+                    tint = MaterialTheme.colorScheme.onSurface,
+                    modifier =
+                        Modifier
+                            .padding(4.dp)
+                            .size(40.dp),
+                )
+            }
+        },
+    )
+}
+
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_NO)
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+fun AppTopBarPreview() {
+    RickAndMortyTheme {
+        AppTopBar(navController = rememberNavController())
+    }
+}
